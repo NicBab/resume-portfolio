7572cbe7eeec09c1ee5ca597e252a725d016ab6e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-09-05 11:35:39 -0500

    contact form complete. emailJS not setup. navbar z indexs adjustment

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,14 +15,14 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite z-[10]
             ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} hover:text-secondary`}
           >
              <Link to={`${nav.path}`}>{nav.title}</Link>
           </li>
         ))}
       </ul>
-      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <div className="sm:hidden flex flex-1 justify-end items-center z-[10]">
         <img
           src={toggle ? close : menu}
           alt="menu"
@@ -33,9 +33,9 @@ const Navbar = () => {
       <div
         className={`${
           toggle ? "flex" : "hidden"
-        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-4 min-w-[140px] rounded-xl sidebar `}
+        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-4 min-w-[140px] rounded-xl sidebar z-[10]`}
       >
-        <ul className="list-none flex flex-col justify-end items-center flex-1 sticky top-0 z-10">
+        <ul className="list-none flex flex-col justify-end items-center flex-1">
           {navLinks.map((nav, index) => (
             <li
               key={nav.id}
