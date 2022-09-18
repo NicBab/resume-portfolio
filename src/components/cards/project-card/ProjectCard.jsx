1a4f2e792406e68fbfd4cbe85865693c0d6b543e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-09-18 16:35:18 -0500

    openInNewTab function working for project title links

--- src/components/cards/project-card/ProjectCard.jsx
+++ src/components/cards/project-card/ProjectCard.jsx
@@ -1,23 +1,28 @@
 import styles from "../../../style";
-import { Link } from "react-router-dom";
 
+//project props from Projects component map 
 const ProjectCard = ({ project }) => {
-  return (
+  const { img, title, description, url, gitHubUrl} = project
+
+  const openInNewTab = () => {
+    const newWindow = window.open(url, '_blank', 'noopener, noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+
+    return (
     <div
       className={`${styles.flexCenter} ${styles.padding} flex-col w-[400px] h-[auto] rounded-[20px] m-10 glass-card`}
     >
       <div
         className={`${styles.sectionImg} h-[250px] w-[250px]`}
       >
-        <img src={project.img} alt="" />
+        <img className="h-[100%] w-[100%]" src={img} alt="" />
       </div>
       <div className={`${styles.cardHeading}`}>
-        <Link to={`#${project.url}`}>
-          <span>{project.title}</span>
-        </Link>
+          <a href={`#${url}`} onClick={() => openInNewTab()}>{title}</a>
       </div>
       <div className={`${styles.paragraph}`}>
-        <p>{project.description}</p>
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,12 @@
 import { ProjectCard } from "../.././components/index/components.index";
-import styles, { layout } from "../.././style"
-import { projectsInfo } from "../.././constants/index"
+import styles, { layout } from "../.././style";
+import { projectsInfo } from "../.././constants/index";
 
 const Projects = () => {
   return (
     <div className={`${styles.padding} bg-primary`}>
-      <section className={`${layout.section} flex flex-wrap`}>
+      <div className="absolute z-[0] w-[100%] h-[100%] rounded-full purple__gradient opacity-[.15]" />
+      <section className={`${layout.section} flex flex-wrap z-[1]`}>
         {projectsInfo.map((project, index) => (
           <ProjectCard project={project} key={index} />
         ))}
