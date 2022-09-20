0b429d77e2dd1196657a4304e5d4cbf3b1de14b1
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-09-20 17:31:46 -0500

    project cards styling

--- src/components/cards/project-card/ProjectCard.jsx
+++ src/components/cards/project-card/ProjectCard.jsx
@@ -13,8 +13,8 @@ const ProjectCard = ({ project }) => {
     <div
       className={`${styles.flexCenter} flex-col w-[200px] h-[250px] sm:w-[300px] sm:h-[500px] rounded-[20px] m-3 z-[1] glass-card shadow`}
     >
-      <div className="w-[190px] h-[190px] sm:w-[300px] sm:h-[500px]">
-        <img className="h-[100%] w-[100%] p-1 sm:p-0 sm:rounded-t-[20px]" src={img} alt="" />
+      <div className="w-[190px] h-[190px] sm:w-[280px] sm:h-[480px] m-3">
+        <img className="h-[100%] w-[100%] p-1" src={img} alt="" />
       </div>
 
       <div className="">
