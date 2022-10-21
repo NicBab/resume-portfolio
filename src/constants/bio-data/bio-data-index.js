728bea0457fb45d1ad0ff5e60da043356aa7d348
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-10-21 07:48:54 -0500

    update bio component

--- src/constants/bio-data/bio-data-index.js
+++ src/constants/bio-data/bio-data-index.js
@@ -5,14 +5,10 @@ export const bioData = [
     },
     {
         id: 2,
-        content: "Graduate of Fullstack Academy 2021, Social Psychology at South Louisiana Community College 2010-2012, and MMI mechanical school in Phoenix, Arizona 2010."
+        content: "With a passion for software and electronics... I have always had the __ to delve into a continuously evolving career and rewarding career path."
     },
     {
         id: 3,
-        content: "Bored with standard educational practices I became eager to be part in a more rewarding and continuously evolving career path.",
+        content: "Graduate of Fullstack Academy 2021, Social Psychology at South Louisiana Community College 2010-2012, and MMI mechanical school in Phoenix, Arizona 2010."
     },
-    {
-        id: 4,
-        content: "With a passion for software and electronics... I enrolled in the Fullstack Academy of software and never looked back."
-    }
 ]
