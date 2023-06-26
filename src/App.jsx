9cee050f0486de9114e7319eed2e3c53bfbe0cce
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2023-06-26 18:44:15 -0500

    fix footer background padding color for responsive screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import styles from "./style";
 import { Routes, Route } from "react-router-dom";
-import {
-  Home,
-  BioPg,
-  PortfolioPg,
-  ContactPg,
-  ResumePg,
-} from "./pages/index/pages.index";
+import { Home, BioPg, PortfolioPg, ContactPg, ResumePg } from "./pages/index/pages.index";
 import { Navbar, Footer } from "./components/index/components.index.js";
 
 function App() {
@@ -23,13 +17,15 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/bio" element={<BioPg />} />
-        <Route path="/portfolio" element={<PortfolioPg />} />
+        <Route path="/portfolio" element={ < PortfolioPg /> } />
         <Route path="/contact" element={<ContactPg />} />
-        <Route path="/resume" element={<ResumePg />} />
+        <Route path="/resume" element={ <ResumePg /> } />
       </Routes>
 
       <div className={`${styles.paddingX} ${styles.flexCenter} bg-primary`}>
-        <Footer />
+        <div className={`${styles.boxWidth}`}>
+          <Footer />
+        </div>
       </div>
     </>
   );
